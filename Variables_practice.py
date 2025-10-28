d957e2eaf68dd8d88b75d1ec4a322a8c4745c30b
muhammad-farhann-ashraf <s45mashr@uni-bonn.de>
2025-10-28 12:26:02 +0100

    Variables practice

---
+++ Variables_practice.py
@@ -0,0 +1,31 @@
+#Assign value to variable
+x=33
+#add variable with value
+print(x+3)
+y=6
+#Add two variables
+print(x+y)
+#Assign new value to same variable
+x=52
+print(x+y)
+#string as variable value
+name="Python"
+print(name)
+print(name + " Learner")
+#Slicing
+print(name[0])
+print(name[1])
+#Negative slicing
+print(name[-1])
+print(name[-4])
+#slicing few characters
+print(name[0:4])
+#open ends
+print(name[:3])
+print(name[3:])
+#length of string
+myname="Muhammad Farhan Ashraf"
+print("length of my name",len(myname))
+#user input
+user_name=input("please enter your name:  ")
+print("length of user name:  ", len(user_name))
