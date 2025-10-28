112801e25bb4a5431f0330df237f272894a861b2
muhammad-farhann-ashraf <s45mashr@uni-bonn.de>
2025-10-28 11:58:10 +0100

    Math operatins practice

---
+++ Math operations.py
@@ -0,0 +1,26 @@
+#Addidition
+print("Addition", 3+2)
+#subtraction
+print("subtraction", 10-2)
+#Division float
+print("Division", 100/5)
+#Division integer
+print("Division integer", 100//5)
+#Multiplication
+print("Multiplication",10*6)
+#Division Reminder
+print("reminder", 35%6)
+#multiple operations
+print("operations", 8+5*7)
+#BODMAS Rule
+print((8+6)*4/3-6)
+#Power rule
+print("Power rule", 2**4)
+#String
+print("Farhan")
+#quotes issue
+print("Farhan's Book")
+#back slash
+print('Farhan\'s "book"')
+#print statment multiple times
+print(5* "farhan")
