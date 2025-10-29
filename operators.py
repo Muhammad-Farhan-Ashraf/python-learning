f2c5b443b05678b43f9a31e27dfd58a1fa0c23ef
muhammad-farhann-ashraf <s45mashr@uni-bonn.de>
2025-10-29 13:48:03 +0100

    Simple math operations

---
+++ operators.py
@@ -0,0 +1,41 @@
+"""Assignment operations"""
+x=5
+y=30
+print(x+y)
+print(x-y)
+print(x/y)
+x+=2
+print(x)
+x*=5
+print(x)
+y/=3
+print(y)
+a,b=65,7
+print(a)
+print(b)
+"""Unary operator"""
+x=45
+print(x)
+-x
+print(-x)
+x=-x
+print(x)
+"""Rational operator"""
+a=5
+b=6
+#check greater
+print(a>b)
+#check less
+print(a<b)
+#check equal
+print(a==b)
+#greater or equal and less or equal
+print(a>=b)
+print(a<=b)
+#not equal to
+print(a!=b)
+"""Logical operator"""
+#and operator
+print(a>4 and b<3)
+#or operator
+print(a>4 or b<3)
