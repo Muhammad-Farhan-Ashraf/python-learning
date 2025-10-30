c07239f61567a00e6de31bd7632dafec5fb6d6f8
muhammad-farhann-ashraf <s45mashr@uni-bonn.de>
2025-10-30 12:35:32 +0100

    user input

---
+++ user_input.py
@@ -0,0 +1,14 @@
+"""user input in python"""
+print("Hy user, lets provide me values ")
+a=input("first number: ")
+b=input("second number:")
+c=(b+a)
+print("addition of input",c)
+print("Sorry i forget to convert your input in to integer, lets do again")
+a=int(input("first number: "))
+b=int(input("second number:"))
+c=(b+a)
+print("addition of input",c)
+print("Multiplication is ", a*b)
+print("division is", a/b)
+
