95d2dff6ca6602dd44152ef054b08c7f4a06d39c
muhammad-farhann-ashraf <s45mashr@uni-bonn.de>
2025-11-07 13:27:46 +0100

    numpy learning

---
+++ numpy_learning.py
@@ -0,0 +1,21 @@
+from numpy import *
+val=array([3,8,9,6])
+print(val)
+a=array([3,5,7],)
+n=array([4,6,8],)
+print(a+n)
+"""Divide arry in equal parts"""
+number=linspace(3,15,20)
+print(number)
+"""arange arry"""
+number=arange(3,20,2)
+print(number)
+"""Zeros"""
+number=zeros(6)
+print(number)
+"""Ones"""
+ones=ones(5)
+print(ones)
+"""logspaced"""
+x=logspace(2,56,3)
+print('%.2f' %x[1])
